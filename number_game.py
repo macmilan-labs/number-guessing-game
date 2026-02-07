ae99a2cc446c226d0bae2ac14ba9493fffb1a2f8
macmilan cyril <macmilancyril@gmail.com>
2026-02-07 13:11:46 +0530

    Add difficulty mode and user input validation

--- number_game.py
+++ number_game.py
@@ -1,14 +1,35 @@
 import random
 
-def play_game():
-    secret = random.randint(1, 100)
-    attempts = 0
-    max_attempts = 7
+def choose_difficulty():
+    print("Choose difficulty:")
+    print("1. Easy (1-50, 10 attempts)")
+    print("2. Medium (1-100, 7 attempts)")
+    print("3. Hard (1-200, 5 attempts)")
 
-    print("\nGuess the number between 1 and 100")
+    choice = input("enter 1, 2, or 3: ")
+
+    if choice == '1':
+        return 50, 10
+    elif choice == '2':
+        return 100, 7
+    else:
+        return 200, 5
+
+def play_game(max_number, max_attempts):
+    secret = random.randint(1, max_number)
+    attempts = 0
+
+    print(f"\nGuess the number between 1 and {max_number}")
 
     while True:
-        guess = int(input("Enter your guess: "))
+        guess_input = input("Enter your guess: ")
+
+        if not guess_input.isdigit():
+            print("Please enter a valid number.")
+            continue
+
+        guess = int(guess_input)
+
         attempts += 1
 
         if attempts >= max_attempts:
@@ -27,7 +48,9 @@ def play_game():
 
 
 while True:
-    play_game()
+    max_number, max_attepmts = choose_difficulty()
+    play_game(max_number, max_attepmts)
+
     again = input("Play again? (y/n): ")
     if again.lower() != 'y':
         print("Thanks for playing!")
