8a9d1bc92d4254b6b91dc80f9a2af7e36d7a7340
macmilan cyril <macmilancyril@gmail.com>
2026-02-07 12:26:19 +0530

    Add attempt limit and game over logic

--- number_game.py
+++ number_game.py
@@ -6,11 +6,19 @@ print("Guess the number between 1 and 100")
 
 while True:
     guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if attempts >= max_attempts:
+        print("Game Over! The number was", secret)
+        break
 
     if guess < secret:
         print("Too low!")
     elif guess > secret:
         print("Too high!")
     else:
-        print("Correct!")
+        print(f"Correct! You guessed in {attempts} attemots.")
         break
+    
+    print("Attempts left:", max_attempts - attempts)
+
